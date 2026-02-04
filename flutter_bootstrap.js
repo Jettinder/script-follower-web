3214e938b662aa40d82d7b328e838f6b5b3e9927
GOAT <info@jettindersingh.com>
2026-02-04 13:28:48 +0100

    Liquid Glass design + improved scrolling

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3427077316"
+    serviceWorkerVersion: "1959151285"
   }
 });
