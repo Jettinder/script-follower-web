9211ad0c3f97f2ef4fdf57a40a137388dc37be29
GOAT <info@jettindersingh.com>
2026-02-04 14:01:05 +0100

    Bright yellow illumination + pulse animation for user turn

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2693190226"
+    serviceWorkerVersion: "3752343823"
   }
 });
